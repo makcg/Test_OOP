d8e3047279a103609caa8520acca729afc32d512
Makc <makcgod1@gmail.com>
2021-03-30 16:30:06 +0300

    test

--- models.py
+++ models.py
@@ -1,5 +1,14 @@
 from random import random
 
+"""
+models.py - class Enemy:
+    свойства - level, lives.
+    конструктор принимает уровень. Уровень жизней противнка = уровень противника.
+    содержит два метода:
+    Статический - select_attack(): возвращает случайное число от одного до трёх.
+    decrease_lives(self): уменьшает количество жизней. Когда жизней становится 0 вызывает исключение EnemyDown.
+"""
+
 
 class Enemy:
     def __init__(self, level, lives):
@@ -14,6 +23,21 @@ class Enemy:
         pass
 
 
+"""
+models.py - class Player:
+    свойства: name, lives, score, allowed_attacks.
+    конструктор принимает имя игрока. Количество жизней указывается из настроек. Счет равен нулю.
+    методы: статический fight(attack, defense) - возвращает результат раунда \n
+    - 0 если ничья, -1 если атака неуспешна, 1 если атака успешна.
+    decrease_lives(self) - то же, что и Enemy.decrease_lives(), вызывает исключение GameOver.
+    attack(self, enemy_obj) - получает ввод от пользователя (1, 2, 3),выбирает атаку противника из объекта enemy_obj;\n
+    вызывает метод fight(); Если результат боя 0 - вывести "It's a draw!", если 1 = "You attacked successfully!"\n
+    и уменьшает количество жизней противника на 1, если -1 = "You missed!"
+    defence(self, enemy_obj) - то же самое, что и метод attack(), только в метод fight первым передается атака \n
+    противника, и при удачной атаке противника вызывается метод decrease_lives игрока.
+"""
+
+
 class Player:
     def __init__(self, name, lives, score, allowed_attacks):
         self.name = name
@@ -24,6 +48,3 @@ class Player:
     @staticmethod
     def fight(attack, defense):
         pass
-
-
-
